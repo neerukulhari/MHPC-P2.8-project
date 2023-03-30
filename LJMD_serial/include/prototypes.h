a6e1b1e035e9614c45d57ad84683a2605d62a9e0
Iara Ota <iara@ota>
2023-03-30 19:34:44 +0200

    removed external C++

--- LJMD_serial/include/prototypes.h
+++ LJMD_serial/include/prototypes.h
@@ -11,27 +11,17 @@
 #include <string.h>
 #include <sys/time.h>
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
 //IO functions
-extern int get_a_line(FILE *fp, char *buf);
-extern void output(mdsys_t *sys, FILE *erg, FILE *traj);
+int get_a_line(FILE *fp, char *buf);
+void output(mdsys_t *sys, FILE *erg, FILE *traj);
 
 //Force functions
-extern void force(mdsys_t *sys);
+void force(mdsys_t *sys);
 
 //Energy functions
-extern void ekin(mdsys_t *sys);
+void ekin(mdsys_t *sys);
 
 //Verlet functions
-extern void velverlet(mdsys_t *sys);
-
-
-#ifdef __cplusplus
-}
-#endif
+void velverlet(mdsys_t *sys);
 
 #endif
