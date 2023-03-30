18cb1c75489ec031661af2a826810aa0f7dafef3
nkulhari <neerajkulhari041993@gmail.com>
2023-03-30 20:28:36 +0200

    extern_added

--- LJMD_serial/include/constants.h
+++ LJMD_serial/include/constants.h
@@ -1,11 +1,11 @@
 #ifndef CONSTANTS_H
-#define CONSTANTS_H
+#define CONSTANTS_H 
 
 /* generic file- or pathname buffer length */
 #define BLEN 200
 
 /* a few physical constants */
-const double kboltz;     /* boltzman constant in kcal/mol/K */
-const double mvsq2e; /* m*v^2 in kcal/mol */
+extern const double kboltz;     /* boltzman constant in kcal/mol/K */
+extern const double mvsq2e; /* m*v^2 in kcal/mol */
 
 #endif
