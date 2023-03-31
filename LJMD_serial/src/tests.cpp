9478a63596e0d41abf63ed8deda76f05d63f85af
Iara Ota <iara@ota>
2023-03-31 12:50:36 +0200

    added velverlet 2-outside

--- LJMD_serial/src/tests.cpp
+++ LJMD_serial/src/tests.cpp
@@ -419,6 +419,95 @@ TEST(VelVerletTest, ThreeParticleInsideCutoff) {
     free(sys.fz);
 }
 
+
+TEST(VelVerletTest, TwoParticleOutsideCutoff) {
+    mdsys_t sys;
+
+    /* set up a two particle system */
+    sys.natoms = 2;
+    sys.rcut = 3.0;
+    sys.box = 10.0;
+    sys.epsilon = 1.0;
+    sys.sigma = 1.0;
+
+    sys.dt = 0.1;
+    sys.mass = 0.1;
+
+    /* allocate memory for the arrays */
+    sys.rx = (double*) malloc(sys.natoms*sizeof(double));
+    sys.ry = (double*) malloc(sys.natoms*sizeof(double));
+    sys.rz = (double*) malloc(sys.natoms*sizeof(double));
+    sys.vx = (double *)malloc(sys.natoms*sizeof(double));
+    sys.vy = (double *)malloc(sys.natoms*sizeof(double));
+    sys.vz = (double *)malloc(sys.natoms*sizeof(double));
+    sys.fx = (double*) malloc(sys.natoms*sizeof(double));
+    sys.fy = (double*) malloc(sys.natoms*sizeof(double));
+    sys.fz = (double*) malloc(sys.natoms*sizeof(double));
+
+    /* set up the positions, at least one large enough to be inside the cutoff */
+    sys.rx[0] = 1.0;
+    sys.ry[0] = 1.0;
+    sys.rz[0] = 1.0;
+    sys.rx[1] = 1.5;
+    sys.ry[1] = 1.5;
+    sys.rz[1] = 4.5;
+    
+    /* set up the velocities to 0*/
+    sys.vx[0] = 0.0;
+    sys.vy[0] = 0.0;
+    sys.vz[0] = 0.0;
+    sys.vx[1] = 0.0;
+    sys.vy[1] = 0.0;
+    sys.vz[1] = 0.0;
+
+    /* set up the forces */
+    sys.fx[0] = 100.0;
+    sys.fy[0] = 100.0;
+    sys.fz[0] = 100.0;
+    sys.fx[1] = 50.0;
+    sys.fy[1] = 50.0;
+    sys.fz[1] = 50.0;
+    
+    /* call the force routine */
+    velverlet(&sys);
+    
+    /* check the results */
+    /* positions updated */
+    EXPECT_NEAR(sys.rx[0], 1.00209, 1e-5);
+    EXPECT_NEAR(sys.ry[0], 1.00209, 1e-5);
+    EXPECT_NEAR(sys.rz[0], 1.00209, 1e-5);
+    EXPECT_NEAR(sys.rx[1], 1.50105, 1e-5);
+    EXPECT_NEAR(sys.ry[1], 1.50105, 1e-5);
+    EXPECT_NEAR(sys.rz[1], 4.50105, 1e-5);
+
+    /* velocities updated */
+    EXPECT_NEAR(sys.vx[0], 0.02092, 1e-5);
+    EXPECT_NEAR(sys.vy[0], 0.02092, 1e-5);
+    EXPECT_NEAR(sys.vz[0], 0.02092, 1e-5);
+    EXPECT_NEAR(sys.vx[1], 0.01046, 1e-5);
+    EXPECT_NEAR(sys.vy[1], 0.01046, 1e-5);
+    EXPECT_NEAR(sys.vz[1], 0.01046, 1e-5);
+
+    /* forces updated */
+    EXPECT_NEAR(sys.fx[0], 0.0, 1e-5);
+    EXPECT_NEAR(sys.fy[0], 0.0, 1e-5);
+    EXPECT_NEAR(sys.fz[0], 0.0, 1e-5);
+    EXPECT_NEAR(sys.fx[1], 0.0, 1e-5);
+    EXPECT_NEAR(sys.fy[1], 0.0, 1e-5);
+    EXPECT_NEAR(sys.fz[1], 0.0, 1e-5);
+
+    /* free the memory */
+    free(sys.rx);
+    free(sys.ry);
+    free(sys.rz);
+    free(sys.vx);
+    free(sys.vy);
+    free(sys.vz);
+    free(sys.fx);
+    free(sys.fy);
+    free(sys.fz);
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
