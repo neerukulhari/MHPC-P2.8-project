7d115833d3aabf6406b503484243290b1433a7ad
nkulhari <neerajkulhari041993@gmail.com>
2023-03-31 15:23:42 +0200

    refactoring_removed

--- LJMD_serial/src/verlet.c
+++ LJMD_serial/src/verlet.c
@@ -4,27 +4,26 @@
 /* velocity verlet propagation step*/
 void velverlet(mdsys_t *sys) 
 {
-    const double factor = 0.5 * sys->dt / (mvsq2e * sys->mass);
-    int N_atoms = sys->natoms;
     int i;
-	for (i = 0; i < N_atoms; ++i) {
-		sys->vx[i] += factor * sys->fx[i];
-		sys->vy[i] += factor * sys->fy[i];
-		sys->vz[i] += factor * sys->fz[i];
-		sys->rx[i] += sys->dt * sys->vx[i];
-		sys->ry[i] += sys->dt * sys->vy[i];
-		sys->rz[i] += sys->dt * sys->vz[i];
-	}
 
-/* compute forces and potential energy */
-force(sys);
-
-/* second part: propagate velocities by another half step */
-for (i=0; i< N_atoms; ++i) {
-    sys->vx[i] += factor * sys->fx[i];
-    sys->vy[i] += factor * sys->fy[i];
-    sys->vz[i] += factor * sys->fz[i];
+    /* first part: propagate velocities by half and positions by full step */
+    for (i=0; i<sys->natoms; ++i) {
+        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
+        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
+        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+        sys->rx[i] += sys->dt*sys->vx[i];
+        sys->ry[i] += sys->dt*sys->vy[i];
+        sys->rz[i] += sys->dt*sys->vz[i];
     }
 
+    /* compute forces and potential energy */
+    force(sys);
+
+    /* second part: propagate velocities by another half step */
+    for (i=0; i<sys->natoms; ++i) {
+        sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
+        sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
+        sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
+    }
 }
 
