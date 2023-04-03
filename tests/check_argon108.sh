7264dd569dfdcbaa5d484e938ccfc4a5e8273058
Iara Ota <iara@ota>
2023-04-03 17:15:14 +0200

    dived the example tests

--- tests/check_argon108.sh
+++ tests/check_argon108.sh
@@ -1,4 +1,3 @@
-../ljmd-serial.x < argon_108.inp
 head -10 argon_108.dat | awk '{printf("%d %.6f %.6f %.6f\n",$1,$2,$3,$4);}'> a.dat
 head -10 ../reference/argon_108.dat | awk '{printf("%d %.6f %.6f %.6f\n",$1,$2,$3,$4);}'> b.dat
 if cmp -s a.dat b.dat; then echo "The argon_108 simulation was executed without errors."; fi
--- tests/check_argon2916.sh
+++ tests/check_argon2916.sh
@@ -1,4 +1,3 @@
-../ljmd-serial.x < argon_2916.inp
 head -10 argon_2916.dat | awk '{printf("%d %.6f %.6f %.6f\n",$1,$2,$3,$4);}'> a.dat
 head -10 ../reference/argon_2916.dat | awk '{printf("%d %.6f %.6f %.6f\n",$1,$2,$3,$4);}'> b.dat
 if cmp -s a.dat b.dat; then echo "The argon_2916 simulation was executed without errors."; fi
