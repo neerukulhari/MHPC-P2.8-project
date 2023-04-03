98a9e56441fab423cacc5eb6e3d35c07496bc778
Iara Ota <iara@ota>
2023-04-03 19:51:17 +0200

    added mpi to main

--- src/main.c
+++ src/main.c
@@ -17,32 +17,50 @@ int main(int argc, char **argv)
     mdsys_t sys;
     double t_start;
 
-    printf("LJMD version %3.1f\n", LJMD_VERSION);
+    #ifdef _MPI
+    MPI_Init(NULL, NULL);
+    #endif
+    fill_mpi_struct(&sys);
+    
+    if (sys.mpirank==0) printf("LJMD version %3.1f\n", LJMD_VERSION);
 
     t_start = wallclock();
 
-    /* read input file */
-    if(get_a_line(stdin,line)) return 1;
-    sys.natoms=atoi(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.mass=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.epsilon=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.sigma=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.rcut=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.box=atof(line);
-    if(get_a_line(stdin,restfile)) return 1;
-    if(get_a_line(stdin,trajfile)) return 1;
-    if(get_a_line(stdin,ergfile)) return 1;
-    if(get_a_line(stdin,line)) return 1;
-    sys.nsteps=atoi(line);
-    if(get_a_line(stdin,line)) return 1;
-    sys.dt=atof(line);
-    if(get_a_line(stdin,line)) return 1;
-    nprint=atoi(line);
+    if(sys.mpirank==0) {
+        /* read input file */
+        if(get_a_line(stdin,line)) return 1;
+        sys.natoms=atoi(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.mass=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.epsilon=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.sigma=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.rcut=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.box=atof(line);
+        if(get_a_line(stdin,restfile)) return 1;
+        if(get_a_line(stdin,trajfile)) return 1;
+        if(get_a_line(stdin,ergfile)) return 1;
+        if(get_a_line(stdin,line)) return 1;
+        sys.nsteps=atoi(line);
+        if(get_a_line(stdin,line)) return 1;
+        sys.dt=atof(line);
+        if(get_a_line(stdin,line)) return 1;
+        nprint=atoi(line);
+    }
+
+    #ifdef _MPI
+    MPI_Bcast(&sys.natoms, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&sys.mass, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&sys.epsilon, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&sys.sigma, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&sys.rcut, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&sys.box, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&sys.nsteps, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&sys.dt, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    #endif
 
     /* allocate memory */
     sys.rx=(double *)malloc(sys.natoms*sizeof(double));
@@ -55,36 +73,61 @@ int main(int argc, char **argv)
     sys.fy=(double *)malloc(sys.natoms*sizeof(double));
     sys.fz=(double *)malloc(sys.natoms*sizeof(double));
 
-    /* read restart */
-    fp=fopen(restfile,"r");
-    if(fp) {
-        for (i=0; i<sys.natoms; ++i) {
-            fscanf(fp,"%lf%lf%lf",sys.rx+i, sys.ry+i, sys.rz+i);
+    #ifdef _MPI
+    sys.cx=(double *)malloc(sys.natoms*sizeof(double));
+    sys.cy=(double *)malloc(sys.natoms*sizeof(double));
+    sys.cz=(double *)malloc(sys.natoms*sizeof(double));
+    #endif
+
+
+    if(sys.mpirank==0) {
+        /* read restart */
+        fp=fopen(restfile,"r");
+        if(fp) {
+            for (i=0; i<sys.natoms; ++i) {
+                fscanf(fp,"%lf%lf%lf",sys.rx+i, sys.ry+i, sys.rz+i);
+            }
+            for (i=0; i<sys.natoms; ++i) {
+                fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
+            }
+            fclose(fp);
+            azzero(sys.fx, sys.natoms);
+            azzero(sys.fy, sys.natoms);
+            azzero(sys.fz, sys.natoms);
+            #ifdef _MPI
+            azzero(sys.cx, sys.natoms);
+            azzero(sys.cy, sys.natoms);
+            azzero(sys.cz, sys.natoms);
+            #endif
+        } else {
+            perror("cannot read restart file");
+            return 3;
         }
-        for (i=0; i<sys.natoms; ++i) {
-            fscanf(fp,"%lf%lf%lf",sys.vx+i, sys.vy+i, sys.vz+i);
-        }
-        fclose(fp);
-        azzero(sys.fx, sys.natoms);
-        azzero(sys.fy, sys.natoms);
-        azzero(sys.fz, sys.natoms);
-    } else {
-        perror("cannot read restart file");
-        return 3;
     }
+    #ifdef _MPI
+    MPI_Bcast(sys.rx, sys.natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(sys.ry, sys.natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(sys.rz, sys.natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(sys.vx, sys.natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(sys.vy, sys.natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(sys.vz, sys.natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    #endif
 
     /* initialize forces and energies.*/
     sys.nfi=0;
     force(&sys);
+
     ekin(&sys);
+    if (sys.mpirank==0) {
 
-    erg=fopen(ergfile,"w");
-    traj=fopen(trajfile,"w");
+        erg=fopen(ergfile,"w");
+        traj=fopen(trajfile,"w");
 
-    printf("Startup time: %10.3fs\n", wallclock()-t_start);
-    printf("Starting simulation with %d atoms for %d steps.\n",sys.natoms, sys.nsteps);
-    printf("     NFI            TEMP            EKIN                 EPOT              ETOT\n");
-    output(&sys, erg, traj);
+        printf("Startup time: %10.3fs\n", wallclock()-t_start);
+        printf("Starting simulation with %d atoms for %d steps.\n",sys.natoms, sys.nsteps);
+        printf("     NFI            TEMP            EKIN                 EPOT              ETOT\n");
+        output(&sys, erg, traj);
+    }
 
     /* reset timer */
     t_start = wallclock();
@@ -94,7 +137,7 @@ int main(int argc, char **argv)
     for(sys.nfi=1; sys.nfi <= sys.nsteps; ++sys.nfi) {
 
         /* write output, if requested */
-        if ((sys.nfi % nprint) == 0)
+        if ((sys.nfi % nprint) == 0 && sys.mpirank==0)
             output(&sys, erg, traj);
 
         /* propagate system and recompute energies */
@@ -102,11 +145,12 @@ int main(int argc, char **argv)
         ekin(&sys);
     }
     /**************************************************/
-
-    /* clean up: close files, free memory */
-    printf("Simulation Done. Run time: %10.3fs\n", wallclock()-t_start);
-    fclose(erg);
-    fclose(traj);
+    if (sys.mpirank==0) {
+        /* clean up: close files, free memory */
+        printf("Simulation Done. Run time: %10.3fs\n", wallclock()-t_start);
+        fclose(erg);
+        fclose(traj);
+    }
 
     free(sys.rx);
     free(sys.ry);
@@ -117,6 +161,12 @@ int main(int argc, char **argv)
     free(sys.fx);
     free(sys.fy);
     free(sys.fz);
+    #ifdef _MPI
+    free(sys.cx);
+    free(sys.cy);
+    free(sys.cz);
+    MPI_Finalize();
+    #endif
 
     return 0;
 }
