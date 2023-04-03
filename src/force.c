abd3b409f43794540090216bb8241615f3e8a340
Iara Ota <iara@ota>
2023-04-03 22:57:45 +0200

    added mpi on force

--- src/force.c
+++ src/force.c
@@ -9,11 +9,22 @@ void force(mdsys_t *sys)
 
     /* zero energy and forces */
     sys->epot=0.0;
+    double epot = 0.0; /* local energy, will be summed over all nodes */
     azzero(sys->fx,sys->natoms);
     azzero(sys->fy,sys->natoms);
     azzero(sys->fz,sys->natoms);
-
+    #ifdef _MPI
+    azzero(sys->cx,sys->natoms);
+    azzero(sys->cy,sys->natoms);
+    azzero(sys->cz,sys->natoms);
+    /* Broadcast the positions to all the nodes */
+    MPI_Bcast(sys->rx, sys->natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(sys->ry, sys->natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(sys->rz, sys->natoms, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    for(i=sys->mpirank; i < (sys->natoms); i += sys->nsize) {
+    #else
     for(i=0; i < (sys->natoms); ++i) {
+    #endif
         for(j=0; j < (sys->natoms); ++j) {
 
             /* particles have no interactions with themselves */
@@ -30,13 +41,28 @@ void force(mdsys_t *sys)
                 ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
                                          +6*pow(sys->sigma/r,6.0)/r);
 
-                sys->epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
+                epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
                                                -pow(sys->sigma/r,6.0));
 
+                #ifdef _MPI
+                sys->cx[i] += rx/r*ffac;
+                sys->cy[i] += ry/r*ffac;
+                sys->cz[i] += rz/r*ffac;
+                #else
                 sys->fx[i] += rx/r*ffac;
                 sys->fy[i] += ry/r*ffac;
                 sys->fz[i] += rz/r*ffac;
+                #endif
             }
         }
     }
+    #ifdef _MPI
+    /* Collect the forces from all the nodes */    
+    MPI_Reduce( sys->cx, sys->fx, sys->natoms, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD );
+    MPI_Reduce( sys->cy, sys->fy, sys->natoms, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD );
+    MPI_Reduce( sys->cz, sys->fz, sys->natoms, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD );
+    MPI_Reduce( &epot, &sys->epot, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD );
+    #else
+    sys->epot = epot;  
+    #endif
 }
