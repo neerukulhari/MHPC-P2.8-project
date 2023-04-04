93e99d6422562a036b35b73565c758e31a3037be
Iara Ota <iara@ota>
2023-04-05 01:16:25 +0200

    used nthreads from struct

--- src/energy.c
+++ src/energy.c
@@ -8,9 +8,6 @@
 
 void ekin(mdsys_t *sys)
 {
-    #ifdef _OPENMP
-    int Nthreads = omp_get_max_threads();
-    #endif
     int i;
     double ekin=0.0;
     sys->ekin=0.0;
@@ -18,7 +15,7 @@ void ekin(mdsys_t *sys)
     int N_atoms = sys->natoms;
     double factor_kb = 2.0/(3.0*N_atoms-3.0)/kboltz;
     #ifdef _OPENMP
-    #pragma omp parallel for num_threads(Nthreads) private(i) reduction(+:ekin) 
+    #pragma omp parallel for num_threads(sys->nthreads) private(i) reduction(+:ekin) 
     #endif
     for (i=0; i<sys->natoms; ++i) {
         ekin += factor * (sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
