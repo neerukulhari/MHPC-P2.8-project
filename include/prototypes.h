6b94eebcc5d95b67f22c20b2f8fb50a447637b38
Iara Ota <iara@ota>
2023-04-04 17:11:04 +0200

    removed commented code

--- include/prototypes.h
+++ include/prototypes.h
@@ -21,7 +21,6 @@ extern int get_a_line(FILE* fp, char* buf);
 extern void output(mdsys_t* sys, FILE* erg, FILE* traj);
 int add_data(FILE * fp, char (*line)[BLEN], char (*restfile)[BLEN], char (*trajfile)[BLEN], char (*ergfile)[BLEN], mdsys_t * sys, int * nprint);
 
-
 //Force functions
 extern void force(mdsys_t *sys);
 extern void force_optimized(mdsys_t *sys);
@@ -35,9 +34,6 @@ extern void ekin(mdsys_t *sys);
 extern void velverlet_update(mdsys_t* sys);
 extern void velverlet_propagation(mdsys_t* sys);
 extern void velverlet(mdsys_t* sys);
-
-// extern void verlet_test_1(mdsys_t *sys);
-// extern void verlet_test_2(mdsys_t *sys);
 
 #ifdef __cplusplus
 }
