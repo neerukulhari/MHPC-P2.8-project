8105f9396a5b6eb871d69e10c245fb6d08f351aa
Stack7 <andrea.stac.sp@gmail.com>
2023-04-04 12:02:19 +0200

    added omp in the energy.c

--- src/energy.c
+++ src/energy.c
@@ -2,20 +2,23 @@
 
 #include "prototypes.h"
 #include "constants.h"
-
 #ifdef _OPENMP
 #include <omp.h>  
-//int Nthreads = atoi(argv[1]);
 #endif
 void ekin(mdsys_t *sys)
 {
     int i;
+    #ifdef _OPENMP
+    int Nthreads = omp_get_num_threads();
+    #endif
 
+    double ekin=0.0;
     sys->ekin=0.0;
-    #pragma omp parallel for num_threads(Nthreads) private (i) reduction(+:sys->ekin)
+    #pragma omp parallel for num_threads(Nthreads) private (i) reduction(+:ekin)
     for (i=0; i<sys->natoms; ++i) {
-        sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
+        ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
     }
+    sys->ekin = ekin;
+    //sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
     sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
 }
-
