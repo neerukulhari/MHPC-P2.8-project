4351243795573203ddc9379d79c26bcf519d34cf
Iara Ota <iara@ota>
2023-04-04 17:11:19 +0200

    indention on comment

--- include/constants.h
+++ include/constants.h
@@ -6,6 +6,6 @@
 
 /* a few physical constants */
 extern const double kboltz;     /* boltzman constant in kcal/mol/K */
-extern const double mvsq2e; /* m*v^2 in kcal/mol */
+extern const double mvsq2e;     /* m*v^2 in kcal/mol */
 
 #endif
