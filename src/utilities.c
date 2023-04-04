cc51dce09239fcf52ffe10fe54944b2220c20004
Stack7 <andrea.stac.sp@gmail.com>
2023-04-04 19:47:05 +0200

    utilities.c files done it

--- src/utilities.c
+++ src/utilities.c
@@ -1,10 +1,14 @@
 #include "utilities.h"
 #include <sys/time.h>
 #include <time.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 /* helper function: get current time in seconds since epoch */
 double wallclock() 
 {
+   
 	struct timeval t; 
 	gettimeofday(&t, 0);
 	return ((double)t.tv_sec) + 1.0e-6 * ((double)t.tv_usec);
@@ -12,8 +16,14 @@ double wallclock()
 
 /* helper function: zero out an array */
 void azzero(double* d, const int n) 
-{       
+{    
+    #ifdef _OPENMP
+      int Nthreads = omp_get_max_threads();
+    #else
+        int Nthreads = 1;
+     #endif
     int i;
+    #pragma omp parallel for num_threads(Nthreads) private(i)
     for (i=0; i<n; ++i) 
 	{
         d[i]=0.0;
