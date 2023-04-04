88e86508c633470a4492ab3d9a452a035f6c38a4
Stack7 <andrea.stac.sp@gmail.com>
2023-04-04 18:20:05 +0200

    after restarting energy.c works!

--- src/energy.c
+++ src/energy.c
@@ -3,22 +3,23 @@
 #include "prototypes.h"
 #include "constants.h"
 #ifdef _OPENMP
-#include <omp.h>  
+ #include <omp.h>
 #endif
+
 void ekin(mdsys_t *sys)
 {
-    int i;
     #ifdef _OPENMP
-    int Nthreads = omp_get_num_threads();
+      int Nthreads = omp_get_max_threads();
+     #else
+    int Nthreads = 1;
     #endif
-
+    int i;
     double ekin=0.0;
     sys->ekin=0.0;
-    #pragma omp parallel for num_threads(Nthreads) private (i) reduction(+:ekin)
+    #pragma omp parallel for num_threads(Nthreads) private(i) reduction(+:ekin) 
     for (i=0; i<sys->natoms; ++i) {
         ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
     }
     sys->ekin = ekin;
-    //sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
     sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
 }
