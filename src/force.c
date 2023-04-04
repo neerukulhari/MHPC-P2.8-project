2cdc418272d6d2f2f5ee73a6722c3e756f1790af
Stack7 <andrea.stac.sp@gmail.com>
2023-04-04 21:40:47 +0200

    forces done it! force.c is ready with omp

--- src/force.c
+++ src/force.c
@@ -1,18 +1,27 @@
 #include "prototypes.h"
 #include "utilities.h"
-
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 void force(mdsys_t *sys)
 {
+    #ifdef _OPENMP
+      int Nthreads = omp_get_max_threads();
+    #else
+        int Nthreads = 1;
+    #endif
+
     double r,ffac;
     double rx,ry,rz;
     int i,j;
 
     /* zero energy and forces */
     sys->epot=0.0;
+    double epot = 0.0;
     azzero(sys->fx,sys->natoms);
     azzero(sys->fy,sys->natoms);
     azzero(sys->fz,sys->natoms);
-
+    #pragma omp parallel for num_threads(Nthreads) private(i,j,r,ffac,rx,ry,rz) reduction(+:epot)
     for(i=0; i < (sys->natoms); ++i) {
         for(j=0; j < (sys->natoms); ++j) {
 
@@ -30,7 +39,7 @@ void force(mdsys_t *sys)
                 ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r
                                          +6*pow(sys->sigma/r,6.0)/r);
 
-                sys->epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
+                epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)
                                                -pow(sys->sigma/r,6.0));
 
                 sys->fx[i] += rx/r*ffac;
@@ -39,4 +48,5 @@ void force(mdsys_t *sys)
             }
         }
     }
+    sys->epot = epot;
 }
