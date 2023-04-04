0cde64808df36e40c3ac027a04cb26696d6b7024
Stack7 <andrea.stac.sp@gmail.com>
2023-04-04 19:28:01 +0200

    added verlet.c omp

--- src/verlet.c
+++ src/verlet.c
@@ -1,12 +1,21 @@
 #include "prototypes.h"
 #include "constants.h"
+#ifdef _OPENMP
+ #include <omp.h>
+#endif
 
 /* velocity verlet propagation step*/
 void velverlet(mdsys_t *sys) 
 {
+    #ifdef _OPENMP
+      int Nthreads = omp_get_max_threads();
+     #else
+        int Nthreads = 1;
+    #endif
     int i;
 
     /* first part: propagate velocities by half and positions by full step */
+   #pragma omp parallel for num_threads(Nthreads) private(i)
     for (i=0; i<sys->natoms; ++i) {
         sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
         sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
@@ -20,10 +29,10 @@ void velverlet(mdsys_t *sys)
     force(sys);
 
     /* second part: propagate velocities by another half step */
+    #pragma omp parallel for num_threads(Nthreads) private(i)
     for (i=0; i<sys->natoms; ++i) {
         sys->vx[i] += 0.5*sys->dt / mvsq2e * sys->fx[i] / sys->mass;
         sys->vy[i] += 0.5*sys->dt / mvsq2e * sys->fy[i] / sys->mass;
         sys->vz[i] += 0.5*sys->dt / mvsq2e * sys->fz[i] / sys->mass;
     }
 }
-
